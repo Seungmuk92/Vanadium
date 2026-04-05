76fcc13d249033ac45fe16a76105acc8d7b0a323
seungmuk92 <seungmuk92@gmail.com>
2026-04-05 18:11:43 +0900

    Web에 API 연동

--- Vanadium.Note.REST/Program.cs
+++ Vanadium.Note.REST/Program.cs
@@ -2,6 +2,14 @@ using Vanadium.Note.REST.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:7700", "https://localhost:7701")
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
 builder.Services.AddControllers();
 builder.Services.AddSingleton<NoteService>();
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
 
