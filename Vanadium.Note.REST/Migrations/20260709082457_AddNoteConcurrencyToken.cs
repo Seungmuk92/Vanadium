5234ca63342d3a8249dbdcaa3b927efcc3b6ac51
smoh <seungmuk92@gmail.com>
2026-07-09 17:38:03 +0900

    #130 노트 동시성 검사를 DB 레벨 토큰으로 전환 (#173)
    * #130 노트 동시성 검사를 DB 레벨 토큰으로 전환
    
    NoteItem.UpdatedAt에 [ConcurrencyCheck]를 추가해 EF Core가 UPDATE/DELETE의
    WHERE 절에 동시성 토큰을 포함하도록 한다. NoteService.Update는 클라이언트가
    알고 있던 버전을 토큰의 OriginalValue로 지정한 뒤 DbUpdateConcurrencyException을
    잡아 409 Conflict로 변환한다. 읽기-쓰기 사이 TOCTOU 구간의 lost update를 DB가
    직접 차단하며, 인메모리 비교를 대체한다. UpdatedAt이 default인 강제 저장은
    토큰을 DB 값 그대로 두어 기존 우회 동작을 유지한다.
    
    마이그레이션(AddNoteConcurrencyToken)은 토큰이 모델 메타데이터라 DDL이 없고
    모델 스냅샷에 IsConcurrencyToken을 기록한다.
    
    * #130 노트 동시성 토큰 회귀 테스트 추가
    
    두 컨텍스트가 같은 행을 수정하는 경쟁 상황에서 DbUpdateConcurrencyException이
    DB 레벨에서 발생함을 검증하고, 서비스 표면에서 오래된 클라이언트 버전은 409
    Conflict, 일치하는 버전은 성공, default 버전은 강제 저장으로 우회됨을 확인한다.

---
+++ Vanadium.Note.REST/Migrations/20260709082457_AddNoteConcurrencyToken.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vanadium.Note.REST.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNoteConcurrencyToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
