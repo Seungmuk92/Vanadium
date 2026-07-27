a07ef7193390c0a7de7f8fe84045bcbd77520585
Seungmuk92 <Seungmuk92@gmail.com>
2026-07-27 09:12:27 +0900

    #350 Tiptap 에디터 ProseMirror 중복 로드로 인한 초기화 예외 수정
    커스텀 노드/익스텐션이 prosemirror-state@1.4.4, prosemirror-view@1.42.1을
    직접 import 하면서 @tiptap/core가 내부적으로 로드하는 @tiptap/pm(prosemirror
    1.42.2)와 별개의 복사본이 로드됐다. CollapsibleHeading 플러그인이 한쪽
    prosemirror-view로 만든 DecorationSet을 다른 쪽 EditorView가 소비하면서
    'Cannot read properties of undefined (reading localsInner/eq)' 예외가 발생했다.
    
    모든 prosemirror import를 @tiptap/core와 동일한 peer 의존성인
    @tiptap/pm@2/state, @tiptap/pm@2/view 경유로 변경해 단일 복사본을 공유하도록 수정.

--- Vanadium.Note.Web/wwwroot/js/tiptap/nodes/collapsible-heading.js
+++ Vanadium.Note.Web/wwwroot/js/tiptap/nodes/collapsible-heading.js
@@ -1,5 +1,5 @@
-import { PluginKey, Plugin, Selection } from 'https://esm.sh/prosemirror-state@1.4.4'
-import { Decoration, DecorationSet } from 'https://esm.sh/prosemirror-view@1.42.1'
+import { PluginKey, Plugin, Selection } from 'https://esm.sh/@tiptap/pm@2/state'
+import { Decoration, DecorationSet } from 'https://esm.sh/@tiptap/pm@2/view'
 import Heading from 'https://esm.sh/@tiptap/extension-heading@2'
 import { toggleOpenAt } from './toggle.js'
 
